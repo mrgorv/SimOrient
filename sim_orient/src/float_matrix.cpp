10fa6ea63f7e35e0fd6b11c21d7bb70fd26fa194
mrgorv <mrgorv@yandex.ru>
2020-02-14 14:07:44 +0300

    Float Matrix Test

--- sim_orient/src/float_matrix.cpp
+++ sim_orient/src/float_matrix.cpp
@@ -21,6 +21,16 @@ public:
 
 	RotationMatrix(vector<vector<float>> m): matrix(m){}
 
+	explicit RotationMatrix(vector<float> ax, float ang){
+		vector<float> x,y,z;
+		float ca=cos(ang);
+		float sa=sin(ang);
+		x = {ca+(1-ca)*ax[0]*ax[0], (1-ca)*ax[0]*ax[1]-sa*ax[2], (1-ca)*ax[0]*ax[2]+sa*ax[1]};
+		y = {(1-ca)*ax[1]*ax[0]+sa*ax[2], ca+(1-ca)*ax[1]*ax[1], (1-ca)*ax[1]*ax[2]-sa*ax[0]};
+		z = {(1-ca)*ax[2]*ax[0]+sa*ax[1], (1-ca)*ax[2]*ax[1]+sa*ax[0], ca+(1-ca)*ax[2]*ax[2]};
+		matrix = {x,y,z};
+	}
+
 	vector<vector<float>> GetMatrix(){
 		return matrix;
 	}
@@ -33,11 +43,27 @@ public:
 		return nv;
 	}
 
+
 	vector<float>& operator[](const int i){return matrix[i];}
 
 private:
 	vector<vector<float>> matrix;
 };
+
+RotationMatrix operator*(RotationMatrix& A, RotationMatrix& B){
+	float x;
+	RotationMatrix R;
+	for(size_t i=0; i<2; i++){
+		for (size_t j=0; j<2; j++){
+			x=0;
+			for(size_t k=0; k<2; k++){
+				x+=A[i][k];
+			}
+			R[i][j]=x;
+		}
+	}
+	return R;
+}
 
 ostream& operator<<(ostream& out, RotationMatrix A){
 	vector<vector<float>> temp = A.GetMatrix();
@@ -48,6 +74,11 @@ ostream& operator<<(ostream& out, RotationMatrix A){
 		}
 		out << "\n";
 	}
+	return out;
+}
+
+ostream& operator<<(ostream& out, const vector<float>& v){
+	for(float i:v)	out << i << ' ';
 	return out;
 }
 
@@ -71,15 +102,53 @@ vector<float> Rotate (vector<float> v, char axis, float ang){
 	return nv;
 }
 
+vector<float> RotateAround(const vector<float> v, vector<float> axis, float ang){
+	RotationMatrix M = RotationMatrix(axis, ang);
+	return M.Apply(v);
+}
+
 int main (){
-	RotationMatrix M({{2,0,0},{0,1,0},{0,0,1}});
-	vector<float> test=M.Apply({3, 4, 5});
-	vector<float> ang_test = Rotate({3, 4, 5}, 'X', 1);
-	for (auto j:test) cout << j << ' ';
-	for (auto i:ang_test) cout << i << ' ';
-//	cout << M;
-//	cout << endl;
-//	M[1][1]=2;
-//	cout << M;
+	vector<float> test = {1,2,1};
+	vector<float> ox = {1,0,0};
+	vector<float> oy = {0,1,0};
+	vector<float> oz = {0,0,1};
+
+	//TEST1 - умножение матриц (из симулятора)
+	float p = 0.6;
+	float k = 0.2;
+	float t = 0.8;
+	RotationMatrix X({{1,0,0},{0,cos(p),sin(p)},{0,-sin(p),cos(p)}});
+	RotationMatrix Y({{cos(k),0,-sin(k)},{0,1,0},{sin(k),0,cos(k)}});
+	RotationMatrix Z({{cos(t),sin(t),0},{-sin(t),cos(t),0},{0,0,1}});
+	RotationMatrix M=X*Y;
+	M=Z*M;
+	vector<float> test1 = M.Apply(test);
+
+	//TEST2 - вращение вокруг осей ССК
+	vector<float> test2 = RotateAround(test, oy, k);
+	test2 = RotateAround(test2, ox, p);
+	test2 = RotateAround(test2, oz, t);
+
+	//TEST3 - вращение вокруг осей ПСК
+	vector<float> nox = ox;
+	vector<float> noy = oy;
+	vector<float> noz = oz;
+	vector<float> test3;
+
+	test3 = RotateAround(test, oy, k);
+	nox = RotateAround(nox, noy, k);
+	noz = RotateAround(noz, noy, k);
+
+	test3 = RotateAround(test3, nox, p);
+	noy = RotateAround(noy, nox, k);
+	noz = RotateAround(noz, nox, k);
+
+	test3 = RotateAround(test3, noz, t);
+	nox = RotateAround(nox, noz, k);
+	noy = RotateAround(noy, noz, k);
+
+	cout << test1 << endl;
+	cout << test2 << endl;
+	cout << test3 << endl;
 	return 0;
 }
